3d442c04ecda326891be558e7aa5c5aa20a56231
yash8mehra <yash8mehra@gmail.com>
2026-01-21 19:01:45 -0500

    just getting it started

---
+++ GradingScheme.java
@@ -0,0 +1,44 @@
+/*************************************************************************
+* Project 1 for CSCI 271-001 Spring 2026
+*
+* Author: Yash Mehra
+* OS: Ubuntu Debian Linux 21.1
+* Compiler: javac 25.0.1
+* Date: January 21, 2026
+*
+* Purpose
+* This program reads a list of scores from an input file, counts how many
+* scores there are in the list, and computes their sum and average.
+*
+*************************************************************************/
+/*******************************************************************
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+* instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+* or any unauthorised sources, including the Internet, either
+* modified or unmodified.
+* - If any source code or documentation used in my program was
+* obtained from other sources, like a text book or course notes,
+* I have clearly indicated that with a proper citation in the
+* comments of my program.
+* - I have not designed this program in such a way as to defeat or
+* interfere with the normal operation of the supplied grading code.
+*
+* <Yash Mehra>
+* <W30677110>
+********************************************************************/
+
+//CamelCase for classes and interfaces
+//camelCase for methods and variables
+//THIS_THING for constants
+
+
+
+
+
+public class GradingScheme {
+    public static void main(String[] args){
+        System.out.println("hi");
+    }
+}
