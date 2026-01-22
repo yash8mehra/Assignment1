a6f88bbe8a73c95c46fd642d9feb3e6455f03444
yash8mehra <yash8mehra@gmail.com>
2026-01-21 20:37:00 -0500

    averages for test and assignments

--- GradingScheme.java
+++ GradingScheme.java
@@ -35,21 +35,49 @@
 
 
 
-
+import java.util.scanner; //for reading in values
 
 public class GradingScheme {
     public static void main(String[] args){
         System.out.println("hi");
 
-
         //making all the variables
-        float FINAL_GRADE = 0.0; //G, The final grade
-        float ASSIGNMENT_AVERAGE = 0.0; // A, the assignment test score average
-        float CLASS_TEST_AVERAGE = 0.0; // T, in class test score average
-        float MIDTERM_GRADE = 0.0; // M, midterm grade
-        float FINAL_EXAM_GRADE = 0.0; // F, Final exam grade
-        float TEST_AVERAGES = 0.0; // E, How well you did on your midterm, final, and in class tests
-        float ASSIGNMENT_WEIGHT = 0.0; // W, Assignment weights
+        double FINAL_GRADE = 0.0; //G, The final grade
+        double ASSIGNMENT_AVERAGE = 0.0; // A, the assignment test score average
+        double CLASS_TEST_AVERAGE = 0.0; // T, in class test score average
+        double MIDTERM_GRADE = 0.0; // M, midterm grade
+        double FINAL_EXAM_GRADE = 0.0; // F, Final exam grade
+        double TEST_AVERAGES = 0.0; // E, How well you did on your midterm, final, and in class tests
+        double ASSIGNMENT_WEIGHT = 0.0; // W, Assignment weights
+        int NUM_ASSIGNMENTS = 7; // 7 assignemnts for averaging
+        int NUM_TESTS = 7; //7 in class tests
+        double ASSIGNMENT_TOTAL = 0.0; // adding up all the assignment scores for averaging
+        double TEST_TOTAL = 0.0; //adding up all the test scores for averaging
+        double ASSIGNMENT_GRADE = 0.0 // used for adding up into assignment total
+        double TEST_GRADE = 0.0 // used for adding up into test total
+
+
+        //I will now read in values for assignment averages
+        Scanner scanner = new scanner(System.in);//scanner to input values
+        system.out.println("Enter 7 assignment scores or 0 if u don't have any left");
+
+
+        for (int i = 0; i < NUM_ASSIGNMENTS; i++){
+            ASSIGNMENT_GRADE = scanner.nextDouble(); //reads the next grade entered
+            ASSIGNMENT_TOTAL += ASSIGNMENT_GRADE;
+        }
+        ASSIGNMENT_AVERAGE = ASSIGNMENT_TOTAL / NUM_ASSIGNMENTS; //divides total grade by 7
+        system.out.println("Your assignment average grade is: " + ASSIGNMENT_AVERAGE);
+
+
+        system.out.println("Enter 7 test scores or 0 if you don't have any left");
+        for (int j = 0; j < NUM_TESTS; j++){
+            TEST_GRADE = scanner.nextDouble();
+            TEST_TOTAL += TEST_GRADE;
+        }
+        CLASS_TEST_AVERAGE = TEST_TOTAL / NUM_TESTS; // divides total test grades by 7
+        system.out.println("Your in class test score average grade is: " + CLASS_TEST_AVERAGE);
+
 
 
 	//calcualtes test average
